960e3dde5dfc65c55a0132a11d8cc3a8b2a41659
Justin Chines <justin.chines@gmail.com>
2013-03-29 22:33:50 -0400

    Fix app launch for heroku

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ if __name__ == '__main__':
   # Bind to PORT if defined, otherwise default to 8000
   update_page()
   port = int(os.environ.get('PORT', 8000))
-  app.run(port=port)
+  app.run(host="0.0.0.0", port=port)
